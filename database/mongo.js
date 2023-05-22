6de9314d4f96a85aac4dfa32b7f4c4c439d32c30
bharathrames <bharath91505@gmail.com>
2023-05-22 16:47:45 +0530

    day37db

---
+++ database/mongo.js
@@ -0,0 +1,204 @@
+db.students.insertMany([
+  {
+    "name":"prasanth",
+    "batch":"B45WD",
+    "qualification":"B.E",
+    "experience" : 2,
+    "taskCompletion":75,
+    "gender":"male"
+  },
+  {
+    "name":"Ajay",
+    "batch":"B42WD",
+    "qualification":"BCA",
+    "experience" : 5,
+    "taskCompletion":80,
+    "gender":"male"
+  },
+  {
+    "name":"Abdur",
+    "batch":"B43WD",
+    "qualification":"BCA",
+    "experience" : 3,
+    "taskCompletion":55,
+    "gender":"male"
+  },
+  {
+    "name":"Anitha",
+    "batch":"B40WD",
+    "qualification":"B.COM",
+    "experience" : 1,
+    "taskCompletion":45,
+    "gender":"female"
+  },
+  {
+    "name":"Anirutha",
+    "batch":"B41WD",
+    "qualification":"BSC",
+    "experience" : 4,
+    "taskCompletion":90,
+    "gender":"female"
+  },
+  {
+    "name":"sivakumar",
+    "batch":"B45WD",
+    "qualification":"B.E",
+    "experience" : 2,
+    "taskCompletion":65,
+    "gender":"male"
+  },
+  {
+    "name":"Aswin",
+    "batch":"B42WD",
+    "qualification":"BCA",
+    "experience" : 5,
+    "taskCompletion":80,
+    "gender":"male"
+  },
+  {
+    "name":"Balaji",
+    "batch":"B43WD",
+    "qualification":"BCA",
+    "experience" : 3,
+    "taskCompletion":55,
+    "gender":"male"
+  },
+  {
+    "name":"Vijipriya",
+    "batch":"B40WD",
+    "qualification":"B.COM",
+    "experience" : 1,
+    "taskCompletion":45,
+    "gender":"female"
+  },
+  {
+    "name":"Sharmila",
+    "batch":"B41WD",
+    "qualification":"BSC",
+    "experience" : 4,
+    "taskCompletion":90,
+    "gender":"female"
+  },
+  {
+    "name":"Venkat",
+    "batch":"B45WD",
+    "qualification":"B.E",
+    "experience" : 2,
+    "taskCompletion":55,
+    "gender":"male"
+  },
+  {
+    "name":"Jeeva",
+    "batch":"B42WD",
+    "qualification":"BCA",
+    "experience" : 5,
+    "taskCompletion":60,
+    "gender":"male"
+  },
+  {
+    "name":"Sathya",
+    "batch":"B43WD",
+    "qualification":"BCA",
+    "experience" : 4,
+    "taskCompletion":55,
+    "gender":"male"
+  },
+  {
+    "name":"Anitha",
+    "batch":"B40WD",
+    "qualification":"B.COM",
+    "experience" : 1,
+    "taskCompletion":45,
+    "gender":"female"
+  },
+  {
+    "name":"Anirutha",
+    "batch":"B41WD",
+    "qualification":"BSC",
+    "experience" : 4,
+    "taskCompletion":90,
+    "gender":"female"
+  },
+  {
+    "name":"Vimal",
+    "batch":"B45WD",
+    "qualification":"B.E",
+    "experience" : 2,
+    "taskCompletion":75,
+    "gender":"male"
+  },
+  {
+    "name":"Nalin",
+    "batch":"B42WD",
+    "qualification":"BCA",
+    "experience" : 5,
+    "taskCompletion":80,
+    "gender":"male"
+  },
+  {
+    "name":"Navin",
+    "batch":"B43WD",
+    "qualification":"BCA",
+    "experience" : 3,
+    "taskCompletion":55,
+    "gender":"male"
+  },
+  {
+    "name":"Radhika",
+    "batch":"B40WD",
+    "qualification":"B.COM",
+    "experience" : 1,
+    "taskCompletion":45,
+    "gender":"female"
+  },
+  {
+    "name":"Ramya",
+    "batch":"B41WD",
+    "qualification":"BSC",
+    "experience" : 4,
+    "taskCompletion":90,
+    "gender":"female"
+  },
+  {
+    "name":"Jeeva",
+    "batch":"B45WD",
+    "qualification":"B.E",
+    "experience" : 2,
+    "taskCompletion":75,
+    "gender":"male"
+  },
+  {
+    "name":"srider",
+    "batch":"B42WD",
+    "qualification":"BCA",
+    "experience" : 5,
+    "taskCompletion":80,
+    "gender":"male"
+  },
+  {
+    "name":"Sathya",
+    "batch":"B43WD",
+    "qualification":"BCA",
+    "experience" : 3,
+    "taskCompletion":55,
+    "gender":"male"
+  },
+  {
+    "name":"Anitha Kaladharan",
+    "batch":"B40WD",
+    "qualification":"B.COM",
+    "experience" : 1,
+    "taskCompletion":45,
+    "gender":"female"
+  },
+  {
+    "name":"Aswini",
+    "batch":"B41WD",
+    "qualification":"BSC",
+    "experience" : 4,
+    "taskCompletion":90,
+    "gender":"female"
+  },
+  ])
+
+  
